c9222564848fe44ab222342493378a5cd54066a3
ellenfel <ellfelfen@gmail.com>
2025-08-13 13:06:58 +0300

    there is a buug anim state doesnt change, vid 2 min 21.50

--- rpg_tutorial_heartbeast/action_rpg/player/player.gd
+++ rpg_tutorial_heartbeast/action_rpg/player/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+var input_vector: = Vector2.ZERO 
+
 func _physics_process(delta: float) -> void:
-	var input_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	input_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
 	velocity = input_vector * SPEED
 	
