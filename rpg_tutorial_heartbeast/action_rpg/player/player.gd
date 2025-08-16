e5520a52fc25b9c2d2f18c67820cd37e634e0cd4
ellenfel <ellfelfen@gmail.com>
2025-08-17 00:13:45 +0300

    solved the stupid bug animations works again

--- rpg_tutorial_heartbeast/action_rpg/player/player.gd
+++ rpg_tutorial_heartbeast/action_rpg/player/player.gd
@@ -4,9 +4,13 @@ const SPEED = 100.0
 
 var input_vector: = Vector2.ZERO 
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+
+
 func _physics_process(delta: float) -> void:
 	input_vector = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	
+
 	velocity = input_vector * SPEED
-	
 	move_and_slide()
